323ceb2269db42e59bb8f59e000b50ad3fe9e7d9
ssi02014 <ssi02014@naver.com>
2023-06-14 02:08:07 +0900

    feat: webview v4

--- webview_basic/lib/main.dart
+++ webview_basic/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:webview_basic/screen/home_screen.dart';
 
 void main() {
+  // Flutter 프레임워크가 앱을 실행할 준비가 될 때까지 기다린다.
+  // WebView v4에서 해당 메서드를 추가해야 한다.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
--- webview_basic/lib/screen/home_screen.dart
+++ webview_basic/lib/screen/home_screen.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse("https://modern-agile-official-client.vercel.app/");
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  HomeScreen({Key? key}) : super(key: key);
 
   // https://blog.codefactory.ai
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('모던 애자일'),
-        backgroundColor: Colors.orange,
-        centerTitle: true,
-      ),
-      body: WebView(
-        initialUrl: 'https://modern-agile-official-client.vercel.app/',
-        javascriptMode: JavascriptMode.unrestricted,
-      )
+        appBar: AppBar(
+          title: Text('모던 애자일'),
+          backgroundColor: Colors.orange,
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  controller.loadRequest(homeUrl);
+                },
+                icon: Icon(Icons.home)),
+          ],
+        ),
+        body: WebViewWidget(
+          controller: controller,
+        )
     );
   }
 }
